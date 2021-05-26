779154be4db84693df14f0f642145060ec964849
Dia-code20 <boughale20@gmail.com>
2021-05-26 14:23:53 +0100

    second commit

--- Bot/app.py
+++ Bot/app.py
@@ -1,6 +1,32 @@
 from chatterbot import ChatBot
-from chatbot import chatbot
 from flask import Flask, render_template, request
+ 
+from chatterbot import ChatBot
+import chatterbot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot(
+    'Chatty',
+    storage_adapter= 'chatterbot.storage.SQLStorageAdapter',
+    logic_adapter = [
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path':'chatterbot.logic.BestMatch',
+            'default_reponse':'Je suis désolé, mais je ne comprends pas, japprends encore',
+            'maximum_similarity_threshold': 0.65
+        }
+    ],
+    preprocessors=[
+        'chatterbot.preprocessors.unescape_html'
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("data/data.yml")
+
 
 app = Flask(__name__)
 app.static_folder = 'static'
